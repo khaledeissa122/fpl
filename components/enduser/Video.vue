1eb8b9f021968241056c3e3911391549f3a34f66
khaledeissa122 <khaledeissa122@outlook.com>
2020-07-29 22:05:03 +0200

    edit in video

--- components/enduser/Video.vue
+++ components/enduser/Video.vue
@@ -12,23 +12,17 @@
               scrolling="no"
               marginheight="0"
               marginwidth="0"
-              width="100%"
-              height="400"
+               width="100%"
+               height="400"
+               allowfullscreen
               type="text/html"
-              src="https://www.youtube.com/embed/IW2NITzbyVc?autoplay=0&fs=0&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0&origin=https://youtubeembedcode.com"
-              >
-              </div>
-            </iframe
+              src="https://www.youtube.com/embed/IW2NITzbyVc?autoplay=0&fs=1&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0&origin=https://youtubeembedcode.com"
+              ></iframe
             >
           </v-container>
         </v-card-text>
         <v-card-actions class="pt-0 mr-7">
-          <v-btn
-            color="teal darken-1"
-            outlined
-            @click="close"
-            >اغلاق</v-btn
-          >
+          <v-btn color="teal darken-1" outlined @click="close">اغلاق</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
--- nuxt.config.js
+++ nuxt.config.js
@@ -25,9 +25,83 @@ export default {
         hid: "description",
         name: "description",
         content: process.env.npm_package_description || ""
-      }
+      },
+      { name: "msapplication-TileColor", content: "#ffffff" },
+      { name: "msapplication-TileImage", content: "/ms-icon-144x144.png" },
+      { name: "theme-color", content: "#ffffff" }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [
+      {
+        rel: "apple-touch-icon",
+        sizes: "57x57",
+        href: "/apple-icon-57x57.png"
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "60x60",
+        href: "/apple-icon-60x60.png"
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "72x72",
+        href: "/apple-icon-72x72.png"
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "76x76",
+        href: "/apple-icon-76x76.png"
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "114x114",
+        hrf: "/apple-icon-114x114.png"
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "120x120",
+        hrf: "/apple-icon-120x120.png"
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "144x144",
+        hrf: "/apple-icon-144x144.png"
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "152x152",
+        hrf: "/apple-icon-152x152.png"
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        hrf: "/apple-icon-180x180.png"
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "192x192",
+        href: "/android-icon-192x192.png"
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png"
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "96x96",
+        href: "/favicon-96x96.png"
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/fav.png"
+      },
+      { rel: "manifest", href: "/manifest.json" }
+    ]
   },
   /*
    ** Global CSS
@@ -63,7 +137,10 @@ export default {
     "https://fantasy.premierleague.com/api/element-summary/176/",
 
     // You can also pass more options
-    ["https://fantasy.premierleague.com/api/element-summary/176/", { ws: false }]
+    [
+      "https://fantasy.premierleague.com/api/element-summary/176/",
+      { ws: false }
+    ]
   ],
   /*
    ** vuetify module configuration
@@ -88,8 +165,8 @@ export default {
           primary: colors.purple,
           secondary: colors.grey.darken1,
           accent: colors.shades.black,
-          error: colors.red.accent3,
-        },
+          error: colors.red.accent3
+        }
       }
     }
   },
